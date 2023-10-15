9eaedb91aa6f98885564ca07cd2207d82b1a91f2
Enric Hernandez Minaya <ehermin@upv.edu.com>
2023-10-15 11:01:56 +0200

    Añadido Node.h

---
+++ Node.h
@@ -0,0 +1,20 @@
+#include <ostream>
+using namespace std;
+
+
+template <typename T> 
+class Node {
+    public:
+      T data;
+      Node<T>* next;
+      Node(T data, Node<T>* next = nullptr){
+	this->data = data;
+	this->next = next;
+      }
+      friend std::ostream& operator<<(std::ostream &out, const Node<T> &node){
+	while(next != nullptr){
+	  out << node->data;
+	}
+	return out;
+      }
+};
