f8f7bf9db3868fcb3c7c3550aaca9f66567ac547
Enric Hernandez Minaya <ehermin@upv.edu.com>
2023-10-16 16:16:59 +0200

    Añadido Circle.h

---
+++ Circle.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "Shape.h"
+using namespace std;
+
+class Circle : public Shape {
+	private:
+	   Point2D center;
+	   double radius;
+
+
+	public:
+	   Circle();
+	   Circle(string color, Point2D center,double radius);
+	   Point2D get_center() const;
+	   void set_center(Point2D p);
+	   double get_radius() const;
+	   void set_radius(double r);
+	   friend std::ostream& operator<<(std::ostream &out, const Circle &c);
+	   double area() const override;
+	   double perimeter() const override;
+	   void translate(double incX, double incY) override;
+	   void print() override;
+
+};
